579c13b53004554b415546bc4bffb6bbc5013d04
rfow <richardfowler0@gmail.com>
2021-01-12 12:18:01 +0000

    Setup file addition.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='Pastor',
+    version="0.1.0",
+    packages=['pastor'],
+    description='Data store for Pandas.',
+    url='https://github.com/rfow/pastor',
+    license='LICENSE.txt',
+    install_requires=['pandas', 'numpy', 'feather-format', 'orjson'],
+)
