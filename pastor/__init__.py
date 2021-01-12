28db1b2b2f75dfd38d748729125b372fef037c00
rfow <richardfowler0@gmail.com>
2021-01-12 12:14:43 +0000

    Implicit adjust.

--- pastor/__init__.py
+++ pastor/__init__.py
@@ -1,4 +1,4 @@
-from .store import store
-from .utils import (set_path, get_path, list_stores, delete_store, delete_stores)
+from pastor.store import store
+from pastor.utils import (set_path, get_path, list_stores, delete_store, delete_stores)
 
 __all__ = ["store", "get_path", "set_path", "list_stores", "delete_store", "delete_stores"]
